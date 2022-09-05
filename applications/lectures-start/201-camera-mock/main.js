35ce38996821c9f046673bab33640b658b3933ef
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-05 18:03:34 +0530

    Update main.js

--- applications/lectures-start/201-camera-mock/main.js
+++ applications/lectures-start/201-camera-mock/main.js
@@ -1,38 +1,25 @@
+import {mockWithVideo, mockWithImage} from '../../libs/camera-mock.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
-    const start = async () => {
+  const start = async() => {
 
-        navigator.mediaDevices.getUserMedia = () => {
-            return new Promise((resolve, reject) => {
-                const video = document.createElement("video");
-                video.setAttribute("sec", "../../assets/mock-videos/course-banner1.mp4");
-                video.setAttribute("loop", "");
+    //mockWithVideo('../../assets/mock-videos/course-banner1.mp4');
+    mockWithImage('../../assets/mock-videos/course-banner1.png');
 
-                video.oncanplay = () => {
-                    video.play();
-                    resolve(video.captureStream());
-                }
-            })
-
-        }
-    // initialize MindAR 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: '../../assets/targets/course-banner.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
-    // create AR object
     const geometry = new THREE.PlaneGeometry(1, 1);
     const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
     const plane = new THREE.Mesh(geometry, material);
 
-    // create anchor
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(plane);
 
-    // start AR
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
       renderer.render(scene, camera);
