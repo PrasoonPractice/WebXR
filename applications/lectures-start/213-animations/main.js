1bacd85299e290a13ae8cff61f1aad46ed10bbf5
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-06 12:08:12 +0530

    Update main.js

--- applications/lectures-start/213-animations/main.js
+++ applications/lectures-start/213-animations/main.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const gltf = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
+    const gltf = await loadGLTF('../../Project1/Avatar.glb');
     gltf.scene.scale.set(0.1, 0.1, 0.1);
     gltf.scene.position.set(0, -0.4, 0);
 
