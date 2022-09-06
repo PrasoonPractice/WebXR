4bf52e8d19655582c757794cda05d22733064c63
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-06 11:50:57 +0530

    Add files via upload

--- applications/lectures-start/213-animations/main.js
+++ applications/lectures-start/213-animations/main.js
@@ -1,8 +1,10 @@
 import {loadGLTF} from "../../libs/loader.js";
+import { mockWithVideo } from '../../libs/camera-mock.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
+    mockWithVideo('../../assets/mock-videos/musicband1.mp4');
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: '../../assets/targets/musicband.mind',
@@ -19,8 +21,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const anchor = mindarThree.addAnchor(0);
     anchor.group.add(gltf.scene);
 
+    //if the model have pre programmed animations they are imported in gltf.animation property
+    //to play these animations we can use the three.js animation mixer class
+    const mixer = new THREE.AnimationMixer(gltf.scene);
+    const action = mixer.clipAction(gltf.animations{0});
+    action.play();
+
+      const clock = new THREE.Clock();
+
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
+      const delta = clock.getDelta();
+      mixer.update(delta);
       renderer.render(scene, camera);
     });
   }
