659d8de55279e04c701282157b9fc7f65d111c64
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-09 15:30:41 +0530

    Update assetload.js

--- applications/assetload.js
+++ applications/assetload.js
@@ -8,14 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.body,
-            imageTargetSrc: '../../assets/targets/musicband.mind',
+            imageTargetSrc: './assets/targets/musicband.mind',
         });
         const { renderer, scene, camera } = mindarThree;
 
         const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
         scene.add(light);
 
-        const gltf = await loadGLTF('../../Project1/Avatar.glb');
+        const gltf = await loadGLTF('./Project1/Avatar.glb');
         gltf.scene.scale.set(1, 1, 1);
         gltf.scene.position.set(-2, -0.2, 0);
 
