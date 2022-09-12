64dd166cfa4a5f6f7dd100aaf41b640aba1c8054
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-12 18:03:24 +0530

    Update portfolioMain.js

--- applications/portfolioMain.js
+++ applications/portfolioMain.js
@@ -1,12 +1,12 @@
 import { CSS3DObject } from './libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js';
-//import {mockWithImage} from './libs/camera-mock.js';
+import {mockWithImage} from './libs/camera-mock.js';
 import { loadGLTF, loadAudio, loadTextures } from './libs/loader.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
     const start = async () => {
 
-       // mockWithImage('./assets/targets/card.png');
+       mockWithImage('./Project1/mock.jpg');
         // initialize MindAR 
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.body,
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
         anchor.group.add(audio);
 
         audio.setBuffer(audioClip);
-        audio.setRefDistance(40);
+        audio.setRefDistance(200);
         audio.setLoop(false);
 
         //anchor.onTargetFound = () => {
