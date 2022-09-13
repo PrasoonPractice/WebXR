1a90d3d8d66bda1a18cf98282e1963a9fa2cb9c1
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-13 15:22:14 +0530

    Update hitTest.js

--- applications/hit/hitTest.js
+++ applications/hit/hitTest.js
@@ -3,33 +3,35 @@ import * as THREE from '../libs/three.js-r132/build/three.module.js';
 import {ARButton} from '../libs/three.js-r132/examples/jsm/webxr/ARButton.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const initialize = async() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+	const initialize = async() => {
+		const scene = new THREE.Scene();
+    		const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-    scene.add(light);
+    		const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    		scene.add(light);
 
-    const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.xr.enabled = true;
-    renderer.setAnimationLoop(() => {
-      renderer.render(scene, camera);
-    });
+    		const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    		renderer.setPixelRatio(window.devicePixelRatio);
+    		renderer.setSize(window.innerWidth, window.innerHeight);
+   		renderer.xr.enabled = true;
+    		renderer.setAnimationLoop(() => {
+			renderer.render(scene, camera);
+		});
 
-    const arButton = ARButton.createButton(renderer, {optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
-    document.body.appendChild(renderer.domElement);
-    document.body.appendChild(arButton);
+		const arButton = ARButton.createButton(renderer, {optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
+    		document.body.appendChild(renderer.domElement);
+    		document.body.appendChild(arButton);
 
-    const controller = renderer.xr.getController(0);
-    scene.add(controller);
-    controller.addEventListener('select', () => {
-	    const avatar = await loadGLTF('./Project1/Avatar.glb');
-	    avatar.scene.quaternion.setFromRotationMatrix(controller.matrixWorld);
-	    avatar.scene.position..applyMatrix4(controller.matrixWorld);
-    });
-  }
+    		const controller = renderer.xr.getController(0);
+    		scene.add(controller);
+		
+		const avatar = await loadGLTF('./Project1/Avatar.glb');
+		
+    		controller.addEventListener('select', () => {
+	    		avatar.scene.quaternion.setFromRotationMatrix(controller.matrixWorld);
+	    		avatar.scene.position.applyMatrix4(controller.matrixWorld);
+    		});
+  	}
 
-  initialize();
+  	initialize();
 });
