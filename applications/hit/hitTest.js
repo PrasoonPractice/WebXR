9d866db3d99a41df52d87798ddf041e2c6f956b8
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-14 05:10:24 +0530

    Update hitTest.js

--- applications/hit/hitTest.js
+++ applications/hit/hitTest.js
@@ -35,13 +35,13 @@ document.addEventListener('DOMContentLoaded', () => {
             messageTexture,
             emailTexture,
         ] = await loadTextures([
-            './Project1/play.png',
-            './Project1/card.png',
-            './Project1/web.png',
-            './Project1/location.png',
-            './Project1/call.png',
-            './Project1/message.png',
-            './Project1/email.png'
+            '../Project1/play.png',
+            '../Project1/card.png',
+            '../Project1/web.png',
+            '../Project1/location.png',
+            '../Project1/call.png',
+            '../Project1/message.png',
+            '../Project1/email.png'
         ]);
 
         const planeGeometry = new THREE.PlaneGeometry(1.63, 0.5);
@@ -175,6 +175,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     } else if (o === webIcon) {
                         window.location.href = " https://falconicx.com/";
                     } else if (o === locationIcon) {
+			window.location.href = " https://www.google.co.in/maps/dir//Good+Earth+City+Center+Mall,+Vikas+Marg,+Pocket+H,+Nirvana+Country,+Sector+50,+Gurugram,+Haryana+122018/";
                         console.log("loc");
                     } else if (o === callIcon) {
                         window.location.href = "tel://+919453275960";
