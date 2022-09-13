c80bb17dd4c31d9436febdce4058825f3c55ef5d
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-13 11:33:07 +0530

    Add files via upload

--- applications/hitTest.js
+++ applications/hitTest.js
@@ -1,75 +1,81 @@
 import * as THREE from './libs/three.js-r132/build/three.module.js';
-import { loadGLTF, loadAudio } from "./libs/loader.js";
-import { CSS3DObject } from './libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js';
-import {ARButton} from './libs/three.js-r132/examples/jsm/webxr/ARButton.js';
+import { ARButton } from './libs/three.js-r132/examples/jsm/webxr/ARButton.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const initialize = async() => {
-    
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera();
+    const initialize = async () => {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera();
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-    scene.add(light);
+        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        scene.add(light);
 
-    const reticleGeometry = new THREE.RingGeometry( 0.15, 0.2, 32 ).rotateX(- Math.PI / 2);
-    const reticleMaterial = new THREE.MeshBasicMaterial(); 
-    const reticle = new THREE.Mesh(reticleGeometry, reticleMaterial);
-    reticle.matrixAutoUpdate = false;
-    reticle.visible = false;
-    scene.add(reticle);
+        const reticleGeometry = new THREE.RingGeometry(0.15, 0.2, 32).rotateX(- Math.PI / 2);
+        const reticleMaterial = new THREE.MeshBasicMaterial();
+        const reticle = new THREE.Mesh(reticleGeometry, reticleMaterial);
+        reticle.matrixAutoUpdate = false;
+        reticle.visible = false;
+        scene.add(reticle);
 
-    const xrRenderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-    xrRenderer.setPixelRatio(window.devicePixelRatio);
-    xrRenderer.setSize(window.innerWidth, window.innerHeight);
-    xrRenderer.xr.enabled = true;
+        const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.xr.enabled = true;
 
-    const arButton = ARButton.createButton(renderer, {requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
-    document.body.appendChild(renderer.domElement);
-    document.body.appendChild(arButton);
+        const arButton = ARButton.createButton(renderer, { requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } });
+        document.body.appendChild(renderer.domElement);
+        document.body.appendChild(arButton);
 
-    const counter = false;
+        const controller = renderer.xr.getController(0);
+        scene.add(controller);
+        controller.addEventListener('select', () => {
+            const avatar = await loadGLTF('./Project1/Avatar.glb');
+            avatar.scene.scale.set(1, 0.85, 1);
+            avatar.scene.visible = false;
 
-    const controller = renderer.xr.getController(0);
-    scene.add(controller);
-    controller.addEventListener('select', () => {
-    //forward to ui
-    consol.log("send to ui.js");
+        });
 
-    });
+        renderer.xr.addEventListener("sessionstart", async (e) => {
+            const session = renderer.xr.getSession();
+            const viewerReferenceSpace = await session.requestReferenceSpace("viewer");
+            const hitTestSource = await session.requestHitTestSource({ space: viewerReferenceSpace });
 
-    xrRenderer.xr.addEventListener("sessionstart", async (e) => {
-      const session = xrRenderer.xr.getSession();
-      const viewerReferenceSpace = await session.requestReferenceSpace("viewer");
-      const hitTestSource = await session.requestHitTestSource({space: viewerReferenceSpace});
+            renderer.setAnimationLoop((timestamp, frame) => {
+                if (!frame) return;
 
+                const hitTestResults = frame.getHitTestResults(hitTestSource);
 
-      xrRenderer.setAnimationLoop((timestamp, frame) => {
-	    if (!frame) return;
+                if (hitTestResults.length) {
+                    const hit = hitTestResults[0];
+                    const referenceSpace = renderer.xr.getReferenceSpace(); // ARButton requested 'local' reference space
+                    const hitPose = hit.getPose(referenceSpace);
 
-	    const hitTestResults = frame.getHitTestResults(hitTestSource);
+                    reticle.visible = true;
+                    reticle.matrix.fromArray(hitPose.transform.matrix);
+                } else {
+                    reticle.visible = false;
+                }
 
-        if (!counter)
-    	    if (hitTestResults.length) {
-	        const hit = hitTestResults[0];
-	        const referenceSpace = xrRenderer.xr.getReferenceSpace(); // ARButton requested 'local' reference space
-	        const hitPose = hit.getPose(referenceSpace);
+                if (reticle.visible && avatar) {
+                    reticle.visible = false;
+                    // stop hit testing
+                    xrHitTestSource.cancel();
+                    xrHitTestSource = null;
+                    // we'll be placing our object right where the reticle was
+                    const pos = reticle.getWorldPosition();
+                    scene.remove(reticle);
+                    avatar.position.set(pos.x, pos.y, pos.z);
+                    avatar.scene.visible = true;
+                }
 
-	        reticle.matrix.fromArray(hitPose.transform.matrix);
-            counter = true;
-            export {hit, reticle};
-	        }
-        renderer.render(scene, camera);
-        cssRenderer.render(cssScene, camera);
-        xrRenderer.render(scene, camera);
-       });
-    });
-    
-    xrRenderer.xr.addEventListener("sessionend", () => {
-      console.log("session end");
-    });
+                renderer.render(scene, camera);
+            });
+        });
 
-  }
+        renderer.xr.addEventListener("sessionend", () => {
+            console.log("session end");
+        });
 
-  initialize();
+    }
+
+    initialize();
 });
