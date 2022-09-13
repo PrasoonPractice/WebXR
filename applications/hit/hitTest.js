78fd235589d56d38d00f17e0fdbc9ac0c4b449b4
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-13 13:26:23 +0530

    Update hitTest.js

--- applications/hit/hitTest.js
+++ applications/hit/hitTest.js
@@ -1,4 +1,5 @@
 import * as THREE from '../libs/three.js-r132/build/three.module.js';
+import { loadGLTF, loadAudio, loadTextures } from './libs/loader.js';
 import { ARButton } from '../libs/three.js-r132/examples/jsm/webxr/ARButton.js';
 
 document.addEventListener('DOMContentLoaded', () => {
