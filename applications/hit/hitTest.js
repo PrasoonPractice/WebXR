ea57287795a96e4e8fe6bb8096d44f14bb8dd268
PrasoonPractice <112545926+PrasoonPractice@users.noreply.github.com>
2022-09-17 02:36:59 +0530

    Update hitTest.js

--- applications/hit/hitTest.js
+++ applications/hit/hitTest.js
@@ -1,7 +1,7 @@
 //import libreries
 import * as THREE from '../libs/three.js-r132/build/three.module.js';
 import {GLTFLoader} from '../libs/three.js-r132/examples/jsm/loaders/GLTFLoader.js';
-import { loadAudio, loadTextures } from '../libs/loader.js';
+import {loadGLTF, loadAudio, loadTextures } from '../libs/loader.js';
 import {ARButton} from '../libs/three.js-r132/examples/jsm/webxr/ARButton.js';
 
 const name = "Prasoon Srivastava";
